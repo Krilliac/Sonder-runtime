9f122edce6d2b58c5ee84c64e7383dd4c4888a8d
Krill <natew94@gmail.com>
2026-08-20 20:25:47 -0500

    Tighten HTTP root import ratchet

--- scripts/check_architecture.py
+++ scripts/check_architecture.py
@@ -54,8 +54,7 @@ UPDATE_ENGINE_PATH = "sonder_runtime/adapters/updates/engine.py"
 VERSION_PLATFORM_PATH = "sonder_runtime/platform/version.py"
 HTTP_SERVE_PATH = "sonder_runtime/interfaces/http/serve.py"
 HTTP_SERVE_ROOT_MODULES = frozenset({
-    "served_action_receipts", "server",
-    "unsafe_lab",
+    "server",
 })
 REPL_PATH = "sonder_runtime/interfaces/repl/repl.py"
 REPL_ROOT_MODULES = frozenset({
